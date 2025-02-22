3512136f2ba247820c94b47ca60f91e72211fb3f
cesarManjaro <cesarwamartinez@gmail.com>
2025-02-22 04:21:16 -0500

    Add better sprites

--- pysoccer/game.py
+++ pysoccer/game.py
@@ -1,80 +1,92 @@
 import pygame
-from models import (Balon,Ghost,Musica,Sonidos,Messi)
+from models import Balon, Kate, Musica, Sonidos, Messi
+
 pygame.init()
 
-FONDOVENTANA=pygame.image.load("./assets/football-pitch.png")
+FONDOVENTANA = pygame.image.load("./assets/football-pitch.png")
 
-colores=(0,0,0)
-RESOLUCION=(780,980)
+colores = (0, 0, 0)
+RESOLUCION = (780, 980)
 
-ventana=pygame.display.set_mode(RESOLUCION)
+ventana = pygame.display.set_mode(RESOLUCION)
 
 pygame.display.set_caption("PySoccer")
 
-ICON=pygame.image.load("./assets/SoccerBall.png")
+ICON = pygame.image.load("./assets/SoccerBall.png")
 pygame.display.set_icon(ICON)
 
 
 musica = Musica("./sounds/aiport.mp3")
 
-collision_balon=Sonidos()
+collision_balon = Sonidos()
 
 
-velocidad=[6,6]    
+velocidad: list = [6, 6]
 
 
-#Instancias Objetos de Display
-player1=Ghost()
-player2=Messi()
-balon=Balon(velocidad=velocidad)
+# Instancias Objetos de Display
+player1: object = Kate()
+player2: object = Messi()
+balon = Balon(velocidad=velocidad)
+puntos = 0
+
+ESTADO_JUGANDO: bool = True
 
 
-ESTADO_JUGANDO=True
 # Collisiones de los personajes y pelotas
-def Collisiones():        
+def Collisiones():
     if player1.rect.colliderect(balon.react):
-        balon.velocidad[1]= -balon.velocidad[1]
+        balon.velocidad[1] = -balon.velocidad[1]
         collision_balon.reproducir("./sounds/8.ogg")
-        print(puntos)
     if player2.rect.colliderect(balon.react):
-        balon.velocidad[1]= - balon.velocidad[1]
+        balon.velocidad[1] = -balon.velocidad[1]
         collision_balon.reproducir("./sounds/8.ogg")
-# Movimientos de los personajes       
+
+
+# Movimientos de los personajes
 def movimientosPersonajes():
     player2.movimientos()
     player1.movimientos()
 
 
-
-puntos=0
+goles_kate: int = 0
+goles_messi: int = 0
 
 
 while ESTADO_JUGANDO:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            ESTADO_JUGANDO=False
-    
+        if event.type == pygame.QUIT:
+            ESTADO_JUGANDO = False
+
     movimientosPersonajes()
     Collisiones()
     balon.actualizar()
     if balon.react.left < 0 or balon.react.right > ventana.get_width():
-        balon.velocidad[0]=-balon.velocidad[0]
+        balon.velocidad[0] = -balon.velocidad[0]
     if balon.react.top < 0 or balon.react.bottom > ventana.get_height():
+        print(ventana.get_height() / 2)
+        if ventana.get_height() / 2 and balon.react.bottom:
+            goles_kate += 1
+            print(f"Gol de Kate {goles_kate}")
+        if ventana.get_height() / 2 and balon.react.top:
+            goles_messi += 1
+            print(f"Gold de Messi {goles_messi}")   
         balon.velocidad[1] = -balon.velocidad[1]
-    boton = pygame.key.get_pressed()
+    boton = pygame.key.get_pressed() 
     if boton[pygame.K_ESCAPE]:
-        ESTADO_JUGANDO = False        
+        ESTADO_JUGANDO = False
+
+    if balon.react.right == 234:
+        ESTADO_JUGANDO = False
 
     ventana.fill(color=colores)
-    ventana.blit(FONDOVENTANA,(0,2))
-    
+    ventana.blit(FONDOVENTANA, (0, 2))
+
     balon.draw(ventanaGame=ventana)
     player1.draw(ventanaGame=ventana)
     player2.draw(ventanaGame=ventana)
 
-    
-    
     pygame.display.flip()
     pygame.time.Clock().tick(90)
 
-pygame.quit()    
+pygame.quit()
--- pysoccer/models.py
+++ pysoccer/models.py
@@ -2,122 +2,142 @@ import pygame
 from typing import Any
 
 
-#  Clase balon que tiene movimiento optimizado 
+#  Clase balon que tiene movimiento optimizado
+
 
 class Balon(pygame.sprite.Sprite):
     def __init__(self, velocidad: list[int]) -> None:
         pygame.sprite.Sprite.__init__(self)
-        self.image=pygame.image.load("./assets/SoccerBall.png")
-        self.react=self.image.get_rect()
-        self.move_ip=self.react.move_ip(0,0)
-        self.velocidad=velocidad
-        self.movimiento=self.react.move(velocidad)
-        
+        self.image = pygame.image.load("./assets/SoccerBall.png")
+        self.react = self.image.get_rect()
+        self.move_ip = self.react.move_ip(0, 0)
+        self.velocidad = velocidad
+        self.movimiento = self.react.move(velocidad)
 
-    #La actualizacion que hace que el balon se mueva
+    # La actualizacion que hace que el balon se mueva
     def actualizar(self) -> None:
-        self.react = self.react.move(self.velocidad)  
-    
-    def draw(self,ventanaGame) -> None:
-        self.ventanaSurface=ventanaGame
-        self.ventanaSurface.blit(self.image,self.react)
+        self.react = self.react.move(self.velocidad)
+
+    def draw(self, ventanaGame) -> None:
+        self.ventanaSurface = ventanaGame
+        self.ventanaSurface.blit(self.image, self.react)
 
 
 # Clase con atributos del FANTASMA
 
-class Ghost(pygame.sprite.Sprite):
+
+class Kate(pygame.sprite.Sprite):
     def __init__(self) -> None:
         pygame.sprite.Sprite.__init__(self)
-        self.images={
-            "ghostLEFT":pygame.image.load("./assets/ghost_left.png"), #Imagen de la izquierda
-            "ghostRIGHT":pygame.image.load("./assets/ghost_RIGHT.png"), # Imagen de la derecha
-            "ghostDefault":pygame.image.load("./assets/ghost_RIGHT.png") # Imagen default
+        self.images = {
+            "kateLeft": pygame.image.load(
+                "./assets/player1/left.png"
+            ),  # Imagen de la izquierda
+            "kateRight": pygame.image.load(
+                "./assets/player1/right.png"
+            ),  # Imagen de la derecha
+            "kateDefault": pygame.image.load(
+                "./assets/player1/default.png"
+            ),  # Imagen default
+            "kateBack": pygame.image.load("./assets/player1/back.png"),
         }
-        self.rect=self.images["ghostDefault"].get_rect()
-        self.rect.move_ip(400,800)
-        self.imagenActual=self.images["ghostDefault"]
-        
-    def draw(self,ventanaGame) -> None:
-        self.ventanaSurface=ventanaGame
-        self.ventanaSurface.blit(self.imagenActual,self.rect)
-    
+
+        self.rect = self.images["kateDefault"].get_rect()
+        self.rect.move_ip(400, 800)
+        self.imagenActual = self.images["kateDefault"]
+
+    def draw(self, ventanaGame) -> None:
+        self.ventanaSurface = ventanaGame
+        self.ventanaSurface.blit(self.imagenActual, self.rect)
+
     def movimientos(self) -> None:
-        self.botones=pygame.key.get_pressed()
+        self.botones = pygame.key.get_pressed()
         if self.botones[pygame.K_LEFT]:
-            self.rect=self.rect.move(-3,0)
-            self.imagenActual=self.images["ghostLEFT"]
+            self.rect = self.rect.move(-3, 0)
+            self.imagenActual = self.images["kateLeft"]
         if self.botones[pygame.K_RIGHT]:
-            self.rect=self.rect.move(3,0)
-            self.imagenActual=self.images["ghostRIGHT"]
+            self.rect = self.rect.move(3, 0)
+            self.imagenActual = self.images["kateRight"]
         if self.botones[pygame.K_UP]:
-            self.rect=self.rect.move(0,-3)
+            self.rect = self.rect.move(0, -3)
+            self.imagenActual = self.images["kateDefault"]
         if self.botones[pygame.K_DOWN]:
-            self.rect=self.rect.move(0,3)
-    
+            self.rect = self.rect.move(0, 3)
+            self.imagenActual = self.images["kateBack"]
 
 
 # Clase con atributos de Messi
 
+
 class Messi(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("./assets/playerDefault.png")
+        self.images = {
+            "lidiaRight" : pygame.image.load("./assets/player2/right.png") ,
+            "lidiaDefault" : pygame.image.load("./assets/player2/default.png"),
+            "lidiaLeft" : pygame.image.load("./assets/player2/left.png") ,
+            "lidiaBack" : pygame.image.load("./assets/player2/back.png") 
+        }
+        self.image = self.images["lidiaDefault"]
         self.rect = self.image.get_rect()
-        self.rect.move_ip(400,1)
-        self.run=False
-
-        
+        self.rect.move_ip(400, 1)
+        self.run = False
     def movimientos(self) -> None:
-        self.botones =pygame.key.get_pressed()
-        if self.botones[pygame.K_a]:  
+        self.botones = pygame.key.get_pressed()
+        if self.botones[pygame.K_a]:
             self.rect = self.rect.move(-3, 0)
-            self.run=True
+            self.run = True
+            self.image = self.images["lidiaLeft"]
         if self.botones[pygame.K_d]:
-            self.rect=self.rect.move(3,0)
-            self.run=True
+            self.rect = self.rect.move(3, 0)
+            self.run = True
+            self.image = self.images["lidiaRight"]
         if self.botones[pygame.K_w]:
-            self.run=True
-            self.rect=self.rect.move(0,-3)
+            self.run = True
+            self.rect = self.rect.move(0, -3)
+            self.image = self.images["lidiaDefault"]
         if self.botones[pygame.K_s]:
-            self.rect=self.rect.move(0,3)
-            self.run=True
-            
-                
-    def draw(self,ventanaGame) -> None:
-        self.ventanaSurface=ventanaGame
-        self.ventanaSurface.blit(self.image,self.rect)
+            self.rect = self.rect.move(0, 3)
+            self.run = True
+            self.image = self.images["lidiaBack"]
+
+    def draw(self, ventanaGame) -> None:
+        self.ventanaSurface = ventanaGame
+        self.ventanaSurface.blit(self.image, self.rect)
+
 
 class SinglentonMetaMusic(type):
-    _instancia=dict()
-    def __call__(cls,*args, **kwargs):
+    _instancia = dict()
+
+    def __call__(cls, *args, **kwargs):
         if cls not in cls._instancia:
-            instancia=super().__call__(*args, **kwargs)
+            instancia = super().__call__(*args, **kwargs)
             cls._instancia[cls] = instancia
         return cls._instancia[cls]
 
 
-
 # Clase para la musica de de fondo o para otras cosas
 class Musica(metaclass=SinglentonMetaMusic):
-    def __init__(self,cancion) -> None:
-        self.cancion=cancion
-        pygame.mixer.init() 
-        pygame.mixer.music.load(cancion)  
+    def __init__(self, cancion) -> None:
+        self.cancion = cancion
+        pygame.mixer.init()
+        pygame.mixer.music.load(cancion)
         self.reproducir()
-        
+
     def reproducir(self) -> None:
-        pygame.mixer.music.play(-1) 
-        
+        pygame.mixer.music.play(-1)
+
 
 # Sonidos class (PRINCIPALMENTE PARA COLLISIONES O DISPAROS)
 
-class Sonidos():
+class Sonidos:
     def __init__(self):
         pygame.mixer.init()
 
-    def reproducir(self,sound):
-        self.sonido=pygame.mixer.Sound(sound)
+    def reproducir(self, sound):
+        self.sonido = pygame.mixer.Sound(sound)
         self.sonido.play()
+        
         
 
 
